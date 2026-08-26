3c27076dfb0e3e25e4ffe9208f2f560a1046b589
Ross <rcnitura@gmail.com>
2026-08-27 01:11:10 +0800

    searching stuff for stasks

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "v19-persistent-storage";
+const CACHE_VERSION = "v21-ongoing-search-only";
 const CACHE_NAME = `field-task-app-${CACHE_VERSION}`;
 
 const BASE_PATH = "/nsca/";
