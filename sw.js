fccf8aa77e061b3fc2da3b48d79c55029537fdf3
Ross <rcnitura@gmail.com>
2026-09-04 12:29:33 +0800

    driver.html checking of driver id

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "v23-payroll-assets";
+const CACHE_VERSION = "v25-driver-list-validation";
 const CACHE_NAME = `field-task-app-${CACHE_VERSION}`;
 
 const BASE_PATH = "/nsca/";
